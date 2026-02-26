26687c6ee3c0590f92c3c7112035cc6349d81dc6
ajaysureserve-tech <ajay.sureserve@gmail.com>
2026-02-26 16:55:26 +0530

    Fix typo in default blog name

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Ajayraj'sReels Guide';
+    : 'Ajayraj Reels Guide';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Next.js Blog Theme';
