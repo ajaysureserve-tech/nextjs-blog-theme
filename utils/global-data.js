6732483680719cee8351a05bf483e011bd52b973
Ajayraj Rajput <ajay.sureserve@gmail.com>
2026-02-27 10:36:12 +0530

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,7 +1,7 @@
 export const getGlobalData = () => {
-const name = process.env.BLOG_NAME ? decodeURI(process.env.BLOG_NAME) : 'Ajay Reels Pro';
-const blogTitle = process.env.BLOG_TITLE ? decodeURI(process.env.BLOG_TITLE) : 'HD Video Downloader';
-const footerText = process.env.BLOG_FOOTER_TEXT ? decodeURI(process.env.BLOG_FOOTER_TEXT) : '© 2026 Ajay Reels Pro';
-
-return { name, blogTitle, footerText };
+return {
+name: 'Ajay Reels Pro',
+blogTitle: 'HD Video Downloader',
+footerText: '© 2026 Ajay Reels Pro',
+};
 };
