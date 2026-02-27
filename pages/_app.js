6030f6e0d7ea9536c37ff990bcdd364eb807558d
Ajayraj Rajput <ajay.sureserve@gmail.com>
2026-02-27 12:53:43 +0530

    Replace Flask app with React component
    Removed Flask API for video downloading and added a React component for the application.

--- pages/_app.js
+++ pages/_app.js
@@ -1,38 +1,7 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import yt_dlp
+import '../styles/globals.css'
 
-app = Flask(__name__)
-CORS(app)
-
-@app.route('/api/download', methods=['GET'])
-def download_video():
-video_url = request.args.get('url')
-if not video_url:
-return jsonify({"error": "URL is required"}), 400
-
-try:
-ydl_opts = {
-'format': 'best',
-'quiet': True,
-'no_warnings': True,
-'check_formats': True,
+function MyApp({ Component, pageProps }) {
+return <Component {...pageProps} />
 }
-with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-info = ydl.extract_info(video_url, download=False)
-download_url = info.get('url')
 
-if not download_url and 'formats' in info:
-download_url = info['formats'][-1]['url']
-
-return jsonify({
-"status": "success",
-"platform": info.get('extractor_key'),
-"title": info.get('title'),
-"download_url": download_url
-})
-except Exception as e:
-return jsonify({"status": "error", "message": str(e)}), 500
-
-if __name__ == '__main__':
-app.run(host='0.0.0.0', port=5000)
+export default MyApp
